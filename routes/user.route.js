17b578fb8be3016cf95e075718b792b5872bf370
Galvin <galvinnguyen1999@gmail.com>
2023-11-01 10:47:34 +0700

    Controller MVC

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,41 +1,16 @@
 const express = require('express')
-const shortid = require('shortid');
-const db = require('../db')
+const controllers = require('../controllers/user.controller') 
 
 const route = express.Router()
-const dbUsers = db.get('users')
 
-route.get('/', (req, res) => {
-  res.render('users/index', {
-    users: dbUsers.value()
-  })
-})
+route.get('/', controllers.index)
 
-route.get('/search', (req, res) => {
-  const q = req.query.q
-  const matchUserList = dbUsers.value().filter((user) => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1 )
-  res.render('users/index', {
-    users: matchUserList,
-    valueInput: q
-  })
-})
+route.get('/search', controllers.search)
 
-route.get('/create', (req, res) => {
-  res.render('users/create')
-})
+route.get('/create', controllers.create)
 
-route.get('/:id', (req, res) => {
-  const id = req.params.id
-  const users = dbUsers.filter((u) => u.id === id).value()
-  res.render('users/view', {
-    users: users
-  })
-})
+route.get('/:id', controllers.get)
 
-route.post('/create', (req, res) => {
-  req.body.id = shortid.generate() 
-  db.get('users').push(req.body).write()
-  res.redirect('/users')
-})
+route.post('/create', controllers.postCreate)
 
 module.exports = route
