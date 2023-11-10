6d29fec20cd1d3bad2574ae42b2ba704cf053e7b
Galvin <galvinnguyen1999@gmail.com>
2023-11-10 23:45:32 +0700

    File Upload

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const controllers = require('../controllers/user.controller') 
 const validate = require('../validate/user.validate')
+const multer  = require('multer')
+
+const upload = multer({ dest: './public/uploads/' })
 
 const route = express.Router()
 
@@ -12,6 +15,6 @@ route.get('/create', controllers.create)
 
 route.get('/:id', controllers.get)
 
-route.post('/create', validate.postCreate , controllers.postCreate)
+route.post('/create',  upload.single('avatar'), validate.postCreate , controllers.postCreate)
 
 module.exports = route
